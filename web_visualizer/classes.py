3340eff9edce65042bef74c687395dee209fd5b3
Ethan Weiner <ethanweiner20@gmail.com>
2021-06-26 11:19:08 -0400

    Routing function improvements

--- web_visualizer/classes.py
+++ web_visualizer/classes.py
@@ -19,14 +19,15 @@ class Router():
             "continent_code": self.continent_code
         }
     # route : Router Router [List-of Router] -> [List-of Router]
-    # Determine a route from _self_ to _destination_, choosing another router from ROUTERS or LANDING_POINTS if needed
+    # Determine a route from _self_ to _destination_, choosing another router from ROUTERS or LANDING_POINTS (if needed)
     # ACCUMULATOR: The _path_ generated so far in the routing process
     # TERMINATION: Dealing with the circular case (adding the same router to the path twice) should ensure that all routers are searched through
     # HEURISTIC: Distance of router from destination
     def route(self, destination, path=[]):
 
-        # Base case: The destination has been reached
+        # Base case: The destination has been reached -> Return accumulator
         if self.latitude == destination.latitude and self.longitude == destination.longitude:
+            path.append(destination)
             return path
         # If we have run into a circular case, the path is invalid
         elif self in path:
@@ -34,24 +35,54 @@ class Router():
         # If the current path is too long, the path is invalid
         elif len(path) > 30:
             return False
+        # Search for a path until it is found, increasing the radius of search as needed
         else:
             # Update the path accumulator
             path.append(self)
-            # Try each router within a given radius, starting with the closest to destination
-            candidate_routers = candidates(self, destination, radius=1)
-
-            for candidate in candidate_routers:
-                candidate_path = candidate.route(destination, path=path)
-                if candidate_path:
-                    return candidate_path
             
-            # If that radius of candidates came out with no successful path, try with a larger radius
+            candidate_path = False
+            radius = 1
+
+            while not candidate_path:
+                candidate_path = self.route_neighbors(destination, radius, path=path)
+                radius += 1
+            
+            return candidate_path
+
+    # route_neighbors : Router Router Number [List-of Router] -> [Maybe List-of Router]
+    # Attempts to find a path (of at most 30 routers) from _origin_ to _destination_, testing all routers within _radius_ of _origin_
+    def route_neighbors(self, destination, radius, path=[]):
+        
+        # sort_routers : [List-of Routers] Router -> _
+        # Sorts _routers_ by distance to _destination_, in ascending order
+        # Improves Efficiency: Forces route_neighbors() to try the closest neighbors first, as opposed to testing the other neighbors
+        def sort_routers(routers, destination):
+            return None
+
+        candidate_routers = self.neighbors(destination, radius)
+        sort_routers(candidate_routers, destination)
+
+        # If the destination is a neighbor, the path is complete
+        if destination in candidate_routers:
+            path.append(destination)    
+            return path
+
+        for candidate in candidate_routers:
+            candidate_path = candidate.route(destination, path=path)
+            if candidate_path:
+                return candidate_path
+
+        return False
+    
+    # neighbors : Router Router Number -> [List-of Routers]
+    # Provides a list of routers within _radius_ of _self_
+    def neighbors(self, radius):
+        return []
 
 
-# candidates : Router Router Number -> [List-of Routers]
-# Provides a list of routers near _origin_, sorted in ascending order by distance to destination
-def candidates(origin, destination, radius=1):
-    return []
+
+
+
 
 
 # INTERPREATION: A landing point for an oceanic cable, defining a path from _start_router_ to _end_router_ via _inner_nodes_
