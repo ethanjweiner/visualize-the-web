92b5a78ee58372a677e356755190faf47d6e3cd4
Ethan Weiner <ethanweiner20@gmail.com>
2021-07-11 07:49:45 -0400

    Routing process refinements

--- web_visualizer/helpers.py
+++ web_visualizer/helpers.py
@@ -1,4 +1,4 @@
-import math
+from math import cos, asin, sqrt, pi
 import pycountry_convert as pc
 
 
@@ -9,9 +9,27 @@ def get_continent(country_code):
 
 
 # distance: Router Router -> Number
-# Determines the distance between _r1_ and _r2_
+# Determines the geographically accurate distance between _r1 and _r2_
+
 def distance(r1, r2):
-    return math.sqrt((float(r1.latitude) - float(r2.latitude)) ** 2 + (float(r1.longitude) - float(r2.longitude)) ** 2)
+    def deg2rad(deg): return deg * (math.pi/180)
+    lat1 = float(r1.latitude)
+    lat2 = float(r2.latitude)
+    lon1 = float(r1.longitude)
+    lon2 = float(r2.longitude)
+
+    p = pi/180
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * \
+        cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+    return 116 * asin(sqrt(a))  # Rough approximation of change in lat/long
+
+    return c
+
+
+# distance_v1: Router Router -> Number
+# Determines the geometrically accurate distance between _r1_ and _r2_
+def distance_v1(r1, r2):
+    return sqrt((float(r1.latitude) - float(r2.latitude)) ** 2 + (float(r1.longitude) - float(r2.longitude)) ** 2)
 
 
 def parse_continent(point_name):
@@ -20,3 +38,10 @@ def parse_continent(point_name):
     country_code = pc.country_name_to_country_alpha2(
         country_name, cn_name_format="lower")
     return get_continent(country_code)
+
+
+# contains_continent_code : String [List-of Point] -> Boolean
+# Does _path_ contain a Point located on the continent specified by _continent_code_?
+
+def contains_continent_code(continent_code, path):
+    return continent_code in map(lambda point: point.continent_code, path)
