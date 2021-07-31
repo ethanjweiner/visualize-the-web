8ee3407cfe64f85c2c84c31ebd86975551c47181
Ethan Weiner <ethanweiner20@gmail.com>
2021-07-31 19:21:17 -0400

    Code review and cleanup

--- web_visualizer/py_auxiliary/temporary.py
+++ web_visualizer/py_auxiliary/temporary.py
@@ -1,67 +1,10 @@
-from web_visualizer import app
-import random
-import urllib.request
-import json
-from flask import jsonify, request, g
-from web_visualizer.classes import Router, LandingPoint, Point, Path
-from web_visualizer.database import Database
-from web_visualizer import db
-from web_visualizer.helpers import *
+from web_visualizer.py_auxiliary.constants import *
 import os
-from itertools import combinations
 
-# Constants
-IP_ADDRESSES_PATH = './web_visualizer/data/ip_addresses.sqlite3'
-LANDING_POINTS_PATH = '/Users/ethanweiner/Downloads/www.submarinecablemap.com-master/public/api/v2/landing-point'
-CABLES_PATH = '/Users/ethanweiner/Downloads/www.submarinecablemap.com-master/public/api/v2/cable'
-
-
-# Routers: Generates GeoJSON locations of _num_routers_ routers
-
-
-@app.route("/routers")
-def routers():
-    # Randomly select ip addresses from table to represent the routers
-    num_routers = request.args.get("num_routers")
-
-    if Router.query.first() != None:
-        Router.query.delete()
-
-    # Generate and store routers in database
-    try:
-        store_routers(num_routers)
-    except Exception:
-        abort(
-            500, description="There was a problem generating the routers. Please refresh.")
-
-    # Query this new set of points from the database
-    return jsonify(list(map(lambda point: point.toJson(), Point.query.all())))
-
-# store_routers : Number -> [List-of Router]
-# Uses the ip_addresses database to generate a list of Routers, of size _num_routers_
-
-
-def store_routers(num_routers):
-
-    ip_addresses_db = Database(IP_ADDRESSES_PATH)
-    routers = []
-
-    # Change to insert num_routers as argument instead (to avoid database manipulation)
-    for ip in ip_addresses_db.query_db('SELECT * FROM ip_addresses ORDER BY RANDOM() LIMIT ?',
-                                       [num_routers]):
-        db.session.add(
-            Router(ip=ip["ip"], latitude=ip["latitude"], longitude=ip["longitude"],
-                   continent_code=ip["continent_id"]))
-
-    db.session.commit()
-
-
-# TEMPORARY DATABASE FUNCTIONS
 
 # store_paths
 # TEMPORARY FUNCTION: Used to initially store the paths into a table
 def store_paths():
-    print("Storing paths...")
 
     for file in os.listdir(CABLES_PATH):
         with open(CABLES_PATH + '/' + file) as cable_file:
