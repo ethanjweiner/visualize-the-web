46eae8f59699ecb358cf6e1d0261d40bea2fe0cf
Ethan Weiner <ethanweiner20@gmail.com>
2021-08-13 12:42:22 -0400

    Gitignore updates

--- web_visualizer/py_main/routers.py
+++ web_visualizer/py_main/routers.py
@@ -6,11 +6,15 @@ from web_visualizer.py_auxiliary.constants import *
 
 from flask import jsonify, request, g, abort
 
+from itertools import combinations
+
 import urllib.request
 import json
-
+import os
 
 # Routers: Generates GeoJSON locations of _num_routers_ routers
+
+
 @app.route("/routers")
 def routers():
 
@@ -28,20 +32,3 @@ def routers():
 
     # Query this new set of points from the database
     return jsonify(list(map(lambda point: point.toJson(), Point.query.all())))
-
-
-# store_routers : Number -> [List-of Router]
-# Uses the ip_addresses database to generate a list of Routers, of size _num_routers_
-def store_routers(num_routers):
-
-    ip_addresses_db = Database(IP_ADDRESSES_PATH)
-    routers = []
-
-    # Change to insert num_routers as argument instead (to avoid database manipulation)
-    for loc in ip_addresses_db.query_db('SELECT * FROM ip_addresses ORDER BY RANDOM() LIMIT ?',
-                                        [num_routers]):
-        db.session.add(
-            Router(ip=loc["ip"], latitude=loc["latitude"], longitude=loc["longitude"],
-                   continent_code=loc["continent_id"]))
-
-    db.session.commit()
