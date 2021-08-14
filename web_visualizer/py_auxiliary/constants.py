8102e76ff3ca0789f134ff7640767b85b4f9c2fb
Ethan Weiner <ethanweiner20@gmail.com>
2021-08-14 18:22:29 -0400

    Mobile display fixes

--- web_visualizer/py_auxiliary/constants.py
+++ web_visualizer/py_auxiliary/constants.py
@@ -1,7 +1,7 @@
 CABLES_GEOJSON_PATH = 'web_visualizer/data/cables.json'
 IP_ADDRESSES_PATH = './web_visualizer/data/ip_addresses.sqlite3'
 MAX_PATH_LENGTH = 30
-MAX_TIME = 5
+MAX_TIME = 3
 LANDING_POINTS_PATH = '/Users/ethanweiner/Downloads/www.submarinecablemap.com-master/public/api/v2/landing-point'
 CABLES_PATH = '/Users/ethanweiner/Downloads/www.submarinecablemap.com-master/public/api/v2/cable'
 TOTAL_NUM_ROUTERS = 8699
