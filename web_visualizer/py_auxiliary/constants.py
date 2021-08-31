b81d28b221bdce6a6758297595e25915027f25ad
Ethan Weiner <ethanweiner20@gmail.com>
2021-08-30 23:06:19 -0400

    Ignore config

---
+++ web_visualizer/py_auxiliary/constants.py
@@ -0,0 +1,6 @@
+CABLES_GEOJSON_PATH = 'web_visualizer/data/cables.json'
+IP_ADDRESSES_PATH = './web_visualizer/data/ip_addresses.sqlite3'
+MAX_PATH_LENGTH = 30
+MAX_TIME = 3
+TOTAL_NUM_ROUTERS = 8699
+TOTAL_NUM_LANDING_POINTS = 1301
